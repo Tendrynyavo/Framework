151996e6be6cc3d968fcd422d793d96ff231abe4
tendrynyavo <tendrirazafimiandrisoa@gmail.com>
2023-03-07 11:18:11 +0300

    mapping in framework

---
+++ WEB-INF/classes/etu2070/framework/Mapping.java
@@ -0,0 +1,8 @@
+package etu2070.framework;
+
+public class Mapping {
+
+    String className;
+    String method;
+    
+}
--- WEB-INF/classes/etu2070/framework/servlet/FrontServlet.java
+++ WEB-INF/classes/etu2070/framework/servlet/FrontServlet.java
@@ -1,15 +1,22 @@
+package etu2070.framework.servlet;
+
 import java.io.*;
+import java.util.HashMap;
+
+import etu2070.framework.Mapping;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
 // Extend HttpServlet class
 public class FrontServlet extends HttpServlet {
 
+    HashMap<String, Mapping> mappingUrls;
+
     public void processRequest(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
         PrintWriter out = response.getWriter();
         out.println(request.getQueryString());
-        // out.println(request.getHttpServletMapping().getMatchValue());
+        out.println(request.getHttpServletMapping().getMatchValue());
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
